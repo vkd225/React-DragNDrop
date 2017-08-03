d9ac6b54b33bdc45b95b078a82c44b07ec52e70a
vkd225 <vkd225@nyu.edu>
2017-08-03 13:30:40 -0400

    adding files

---
+++ src/client/app/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {render} from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Container from './Container.jsx';
+import ImageWidget from './imgwidget.jsx';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
+class App extends React.Component {
+  render () {
+
+    return (
+      <div>
+        <MuiThemeProvider>
+          <Container />
+        </MuiThemeProvider>
+      </div>
+
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
